c95149ad337beb4a0caee288187de66d979a8eef
SebOra-WSEI <LiBroPWSZ@gmail.com>
2023-05-24 23:41:43 +0200

    Add default fetch

--- frontend/hades/src/components/Login/LoginForm.tsx
+++ frontend/hades/src/components/Login/LoginForm.tsx
@@ -1,26 +1,26 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { routes } from '../../utils/routes';
 
 export const LoginForm: React.FC = () => {
-  // const [isLogged, setIsLogged] = useState<boolean>(false);
+  const [isLogged, setIsLogged] = useState<boolean>(false);
 
-  // useEffect(() => {
-  //   axios.post(routes.login, {
-  //     username: 'user',
-  //     password: 'user'
-  //   })
-  //     .then(res => setIsLogged(!!res.data))
-  //     .catch(err => console.log(err))
-  // }, [isLogged])
+  useEffect(() => {
+    axios.post(routes.login, {
+      username: 'user',
+      password: 'user'
+    })
+      .then(res => setIsLogged(!!res.data))
+      .catch(err => console.log(err))
+  }, [isLogged])
 
 
-  axios.post(routes.login, {
-    username: 'user',
-    password: 'user'
-  })
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err))
+  // axios.post(routes.login, {
+  //   username: 'user',
+  //   password: 'user'
+  // })
+  //   .then(res => console.log(res.data))
+  //   .catch(err => console.log(err))
 
   return <>{'Login Component'}</>
 }
