33a637a4917fc5a2412f34ee2d7da216cc1dcbe0
SebOra-WSEI <LiBroPWSZ@gmail.com>
2023-05-24 23:25:50 +0200

    Default settings

---
+++ frontend/hades/src/utils/routes.ts
@@ -0,0 +1,7 @@
+const DEFAULT_ROUTE = 'http://localhost:3000';
+
+export const routes = {
+  login: `${DEFAULT_ROUTE}/login`,
+  register: `${DEFAULT_ROUTE}/register`,
+  users: `${DEFAULT_ROUTE}/users`,
+}
